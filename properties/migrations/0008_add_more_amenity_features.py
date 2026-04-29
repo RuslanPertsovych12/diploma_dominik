5daef80b903359ab7da5ffac8ee18b31b4abac33
Vortex <ruslanpercovic391@gmail.com>
2026-04-29 22:13:49 +0300

    diploma

--- properties/migrations/0008_add_more_amenity_features.py
+++ properties/migrations/0008_add_more_amenity_features.py
@@ -1,4 +1,5 @@
 from django.db import migrations
+from django.utils.text import slugify
 
 FEATURES_TO_ADD = [
     chr(1052)+chr(1086)+chr(1078)+chr(1085)+chr(1072)+chr(32)+chr(1079)+chr(32)+chr(1090)+chr(1074)+chr(1072)+chr(1088)+chr(1080)+chr(1085)+chr(1072)+chr(1084)+chr(1080),
@@ -13,10 +14,28 @@ FEATURES_TO_ADD = [
     'Wi-Fi',
     chr(1061)+chr(1086)+chr(1083)+chr(1086)+chr(1076)+chr(1080)+chr(1083)+chr(1100)+chr(1085)+chr(1080)+chr(1082),
 ]
-def add_features(apps,schema_editor):
-    F=apps.get_model('properties','Feature')
-    for n in FEATURES_TO_ADD: F.objects.get_or_create(name=n)
-def noop(a,b): pass
+
+def add_features(apps, schema_editor):
+    F = apps.get_model('properties', 'Feature')
+
+    for n in FEATURES_TO_ADD:
+        slug = slugify(n)
+
+        F.objects.get_or_create(
+            slug=slug,
+            defaults={"name": n}
+        )
+
+def noop(a, b):
+    pass
+
+
 class Migration(migrations.Migration):
-    dependencies=[('properties','0007_add_featured_until_view_count_contact_verified')]
-    operations=[migrations.RunPython(add_features,noop)]
+
+    dependencies = [
+        ('properties', '0007_add_featured_until_view_count_contact_verified')
+    ]
+
+    operations = [
+        migrations.RunPython(add_features, noop)
+    ]
